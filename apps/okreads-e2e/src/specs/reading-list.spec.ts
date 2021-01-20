e122487f772a2a86da37597e35a50fb41c3aff5b
hemangtungal1 <hemang.tungal@infosys.com>
2021-01-20 18:51:54 +0530

    Task4: implemented changes to mark a book as finished

--- apps/okreads-e2e/src/specs/reading-list.spec.ts
+++ apps/okreads-e2e/src/specs/reading-list.spec.ts
@@ -1,12 +1,14 @@
-import { $, browser, ExpectedConditions } from 'protractor';
+import { $, $$, browser, By, ExpectedConditions } from 'protractor';
 
 describe('When: I use the reading list feature', () => {
-  it('Then: I should see my reading list', async () => {
+  beforeEach(async () => {
     await browser.get('/');
     await browser.wait(
       ExpectedConditions.textToBePresentInElement($('tmo-root'), 'okreads')
     );
+  });
 
+  it('Then: I should see my reading list', async () => {
     const readingListToggle = await $('[data-testing="toggle-reading-list"]');
     await readingListToggle.click();
 
@@ -16,5 +18,51 @@ describe('When: I use the reading list feature', () => {
         'My Reading List'
       )
     );
+  });
+
+  it('Then: I should be able to mark a book as finished', async () => {
+    /* search book */
+    await $('input[type="search"]').sendKeys('Algorithm');
+    await $('form').submit();
+
+    /* add book to reading list */
+    await $$('[data-testing="book-add-button"][ng-reflect-disabled="false"]')
+      .first()
+      .click();
+
+    /* open reading list */
+    await $('[data-testing="toggle-reading-list"]').click();
+
+    /* mark book as finished */
+    let recentlyFinishedBook = $$('.reading-list-item').last();
+    await recentlyFinishedBook.element(By.className('mark-finish-circle')).click();
+
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement(
+        recentlyFinishedBook.all(By.className('finished-text')).first(),
+        'Finished on:'
+      )
+    );
+
+    /* remove book from reading list */
+    await $$('[data-testing="book-remove-button"]').last().click();
+
+    /* close reading list */
+    await $('[data-testing="close-reading-list"]').click();
+
+    /* adding same book again*/
+    await $$('[data-testing="book-add-button"][ng-reflect-disabled="false"]')
+      .first()
+      .click();
+
+    /* open reading list */
+    await $('[data-testing="toggle-reading-list"]').click();
+
+    recentlyFinishedBook = $$('.reading-list-item').last();
+
+    /* finish button should be present again for the same book */
+    expect(
+      await recentlyFinishedBook.all(By.className('mark-finish-circle')).count()
+    ).toEqual(1);
   });
 });
--- libs/books/data-access/src/lib/+state/reading-list.effects.spec.ts
+++ libs/books/data-access/src/lib/+state/reading-list.effects.spec.ts
@@ -4,9 +4,13 @@ import { provideMockActions } from '@ngrx/effects/testing';
 import { provideMockStore } from '@ngrx/store/testing';
 import { HttpTestingController } from '@angular/common/http/testing';
 
-import { SharedTestingModule } from '@tmo/shared/testing';
+import {
+  createReadingListItem,
+  SharedTestingModule
+} from '@tmo/shared/testing';
 import { ReadingListEffects } from './reading-list.effects';
 import * as ReadingListActions from './reading-list.actions';
+import { ReadingListItem } from '@tmo/shared/models';
 
 describe('ToReadEffects', () => {
   let actions: ReplaySubject<any>;
@@ -25,11 +29,11 @@ describe('ToReadEffects', () => {
 
     effects = TestBed.inject(ReadingListEffects);
     httpMock = TestBed.inject(HttpTestingController);
+    actions = new ReplaySubject();
   });
 
   describe('loadReadingList$', () => {
     it('should work', done => {
-      actions = new ReplaySubject();
       actions.next(ReadingListActions.init());
 
       effects.loadReadingList$.subscribe(action => {
@@ -40,6 +44,46 @@ describe('ToReadEffects', () => {
       });
 
       httpMock.expectOne('/api/reading-list').flush([]);
+    });
+  });
+
+  describe('markBookAsFinished$', () => {
+    let item: ReadingListItem;
+    let finishedDate: string;
+
+    beforeAll(() => {
+      item = createReadingListItem('A');
+      finishedDate = new Date().toISOString();
+    });
+
+    it('should add a book to the reading list successfully', (done) => {
+      actions.next(ReadingListActions.markBookAsFinished({ bookId: item.bookId, finishedDate }));
+
+      effects.markBookAsFinished$.subscribe((action) => {
+        expect(action).toEqual(
+          ReadingListActions.markBookAsFinishedSucceeded()
+        );
+        done();
+      });
+
+      httpMock
+        .expectOne(`/api/reading-list/${item.bookId}/finished`)
+        .flush([item]);
+    });
+
+    it('should undo the added book when API returns error', (done) => {
+      actions.next(ReadingListActions.markBookAsFinished({ bookId: item.bookId, finishedDate }));
+
+      effects.markBookAsFinished$.subscribe((action) => {
+        expect(action).toEqual(
+          ReadingListActions.markBookAsFinishedFailed({ bookId: item.bookId })
+        );
+        done();
+      });
+
+      httpMock
+        .expectOne(`/api/reading-list/${item.bookId}/finished`)
+        .flush({}, { status: 500, statusText: 'server error' });
     });
   });
 });
